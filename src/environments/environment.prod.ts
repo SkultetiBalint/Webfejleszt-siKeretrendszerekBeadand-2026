fe2480c110e8af73b3e99ff8144fb87062fbf8bd
SkultetiBalint <skulteti.balint@gmail.com>
2026-05-08 17:11:19 +0200

    deploy javítás

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Cseréld le a saját Render backend URL-jére deploy után, pl.:
   // apiUrl: 'https://nailtime-api.onrender.com'
-  apiUrl: 'https://nailtime-api.onrender.com'
+  apiUrl: 'https://webfejleszt-sikeretrendszerekbeadand-2026.onrender.com'
 };
